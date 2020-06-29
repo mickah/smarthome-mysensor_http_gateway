1cb2009a222124299c699026c3ca480f8b897fc7
michael@fagno.fr <michael@fagno.fr>
2020-06-29 10:07:33 +0200

    :sparkles: turn mysensors data into json

--- app/SensorsController.py
+++ app/SensorsController.py
@@ -1,25 +1,89 @@
 import mysensors.mysensors as mysensors
 import json
 import os
-
-def event(message):
-    """Callback for mysensors updates."""
-    print('sensor_update ' + str(message.node_id))
+from datetime import datetime, timezone
 
 
 class SensorsController:
+    """ Sensors Controller class. It handles all interactions with mysensors """
+
     def __init__(self):
-        #
-        serial_port = os.environ['MYSENSOR_SERIAL']
+        serial_port = os.environ["MYSENSOR_SERIAL"]
         if serial_port == None:
             print("MYSENSOR_SERIAL env variable missing!")
         else:
-            print("Using serial port: "+str(os.environ['MYSENSOR_SERIAL']))
-        self.gateway = mysensors.SerialGateway(os.environ['MYSENSOR_SERIAL'], baud=115200,  event_callback=event, persistence=True,  persistence_file='./mysensors.pickle', protocol_version='2.2')
+            print("Using serial port: " + str(os.environ["MYSENSOR_SERIAL"]))
+
+        self.gateway = None
+        self.gateway = mysensors.SerialGateway(
+            os.environ["MYSENSOR_SERIAL"],
+            baud=38400,
+            event_callback=self.event,
+            persistence=True,
+            persistence_file="./mysensors.pickle",
+            protocol_version="2.2",
+        )
         self.gateway.start_persistence()
         self.gateway.start()
-        #self.sensors_tests = [{"sensor_id":6,"type":16,"children":[{"id":2,"type":"S_HUM","values":[{"V_HUM":60}]}]}]
-
 
     def getSensors(self):
         return self.gateway.sensors.values()
+
+    def event(self, message):
+        """Callback for mysensors updates"""
+        if (
+            self.gateway
+            and message.node_id != 0
+            and message.child_id != 255
+            and message.type in [1, 2]
+            and self.gateway.is_sensor(message.node_id, message.child_id)
+        ):
+            child = self.gateway.sensors[message.node_id].children[message.child_id]
+
+            if message.sub_type in [
+                0,
+                1,
+                3,
+                4,
+                6,
+                7,
+                8,
+                9,
+                10,
+                11,
+                12,
+                13,
+                14,
+                17,
+                18,
+                23,
+                34,
+                35,
+                37,
+                38,
+                39,
+            ]:
+                """ Send float types as float"""
+                payload = float(message.payload)
+            elif message.sub_type in [
+                2,
+                15,
+                16,
+            ]:
+                """ Send int types as int"""
+                payload = int(message.payload)
+            else:
+                payload = message.payload
+
+            child_json = {
+                "node_id": message.node_id,
+                "child_id": message.child_id,
+                "child_type": child.type,
+                "data_type": message.sub_type,
+                "payload": payload,
+            }
+
+            if message.type == 1:
+                print("sensor_updated: {}".format(json.dumps(child_json)))
+            elif message.type == 2:
+                print("sensor_request: {}".format(json.dumps(child_json)))
--- app/routes.py
+++ app/routes.py
@@ -1,15 +1,47 @@
 from app import app
 import json
+from flask import Response
 from .SensorsController import SensorsController
 
+# Create the controller instance
 sensors_controller = SensorsController()
 
-@app.route('/')
-@app.route('/index')
-def index():
-    return "Welcome to mysensor API server"
 
-@app.route('/sensors', methods=['GET'])
+@app.route("/")
+@app.route("/index")
+def index():
+    return "Welcome to mysensors API server"
+
+
+@app.route("/sensors", methods=["GET"])
 def sensors():
-    print(sensors_controller.getSensors())
-    return json.dumps(list(sensors_controller.getSensors()))
+    # List all sensors as JSON
+    sensors_json = {}
+    for node in sensors_controller.getSensors():
+        child_list = []
+        for ch_id in node.children:
+            child = node.children[ch_id]
+            child_list.append(
+                {
+                    "child_id": ch_id,
+                    "child_type": child.type,
+                    "description": child.description,
+                    "values": child.values,
+                }
+            )
+
+        node_json = {
+            "sensor_id": node.sensor_id,
+            "sketch_name": node.sketch_name,
+            "sketch_version": node.sketch_version,
+            "battery_level": node.battery_level,
+            "heartbeat": node.heartbeat,
+            "protocol_version": node.protocol_version,
+            "children": child_list,
+        }
+        sensors_json[node.sensor_id] = node_json
+
+    resp = Response(
+        response=json.dumps(node_json), status=200, mimetype="application/json"
+    )
+    return resp
